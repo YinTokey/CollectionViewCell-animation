b3a42699898c4b1c64c4b8246e025f3c16e9e2b1
Yinj <YinjChen@163.com>
2016-05-19 16:00:04 +0800

    basic animation

---
+++ CollectionViewCell-Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewCell-Animation
+//
+//  Created by Mac on 16/5/19.
+//  Copyright © 2016年 YinTokey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewCell-Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewCell-Animation
+//
+//  Created by Mac on 16/5/19.
+//  Copyright © 2016年 YinTokey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
